2046629c4643491d7338b54ce0a3973773ef6003
fengguangze <812036047@qq.com>
2019-12-17 14:23:33 +0800

    first commit

---
+++ Example/Pods/Target Support Files/Pods-RamboModulePodTest_Example/Pods-RamboModulePodTest_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RamboModulePodTest_Example : NSObject
+@end
+@implementation PodsDummy_Pods_RamboModulePodTest_Example
+@end
---
+++ Example/Pods/Target Support Files/RamboModulePodTest/RamboModulePodTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RamboModulePodTest : NSObject
+@end
+@implementation PodsDummy_RamboModulePodTest
+@end
